c4af92be08d68eb319bb43e9d70a5415a8e7e05c
Spazcool <douglas.wrightiii@gmail.com>
2017-11-29 16:16:28 +0800

    loading animation & style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import './loading.css'
 import fetch from './fetchData'
 import Usercard from './Usercard'
 import Buts from './Buts'
@@ -16,38 +17,48 @@ class App extends Component {
   componentDidMount () {
     fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
       .then(function (result) {
-        console.log('Alltime Loaded...', result.length)
+        console.log('Alltime Users Loaded.', result.length)
         this.setState({alltime: result})
       }.bind(this))
 
     fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
       .then(function (result) {
-        console.log('Recent Loaded...', result.length)
+        console.log('Recent Users Loaded.', result.length)
         this.setState({recent: result})
       }.bind(this))
-      // .catch(function () {
-      //   return (
-      //     console.log('Errors')
-      //   )
-      // })
+      .catch(function () {
+        return (
+          console.log('Errors')
+        )
+      })
   }
   handleClick () {
     this.setState({alltimeToggle: !this.state.alltimeToggle})
   }
   render () {
     if (this.state.alltime && this.state.recent) {
-      console.log('Rendering it')
+      console.log('Rendering Users.')
       return (
         <div className='App'>
           <header className='App-header'>
+            <h1>The Top Dogs</h1>
             <Buts onClick={this.handleClick.bind(this)} label={this.state.alltimeToggle.toString()} />
           </header>
           <Usercard alltime={this.state.alltime} recent={this.state.recent} alltimeToggle={this.state.alltimeToggle} />
-          <footer className='footer' />
+          <footer className='footer'>
+            Built by <a href='http://www.spazcool.com'>Doug Wright</a>.
+            <br />
+            Loading animation by <a href='http://danielhearn.co.uk/'>Daiel Hearn</a>.
+          </footer>
         </div>
       )
     }
-    return <div>Loading...</div>
+    return <div className='loaderContainer'>
+      <div className='loader'>
+        <div id='largeBox' />
+        <div id='smallBox' />
+      </div>
+    </div>
   }
 }
 
